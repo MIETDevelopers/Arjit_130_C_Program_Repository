250ecfd9feb70e9be6274f68d2e86043bb2fb7ea
arjit158 <76278420+arjit158@users.noreply.github.com>
2021-03-18 17:05:37 +0530

    oddeven

---
+++ oddeven.c
@@ -0,0 +1,21 @@
+//Author:Arjit Manhas
+//Creation Date:18/3/2021
+//Last Modification:18/3/2021
+//Purpose Of The Program: To Determine Whether The Entered Number Is Even or Odd
+#include <stdio.h>
+int main(){//Main Function Body 
+    
+    int num;//Data type and Variable Declaration
+    
+    printf("Enter an integer: ");//Print Function Calling
+    
+    scanf("%d", &num);//taking user input in integer data type
+	//Using If Else condition
+    if(num % 2 == 0)
+    
+        printf("%d is even.", num);//Print Function Calling
+    else
+        printf("%d is odd.", num);//Print Function Calling
+    
+    return 0;
+}
