dcd16ee4d5776f6defd5222bc7c2bfab77fbd0f9
arjit158 <76278420+arjit158@users.noreply.github.com>
2021-03-18 17:31:53 +0530

    Update area.c

--- area.c
+++ area.c
@@ -4,31 +4,31 @@
 //Purpose of the program:To check Area and Perimeter of Circle, Square and Rectangle
 #include <stdio.h> 
 int main() {
-   float area, perimeter, side, radius, length, breadth;
+   float area, perimeter, side, radius, length, breadth;//Declarartion of data type and variables
 
-    perimeter = 2*3.14*radius;
-    scanf("%f = 2*3.14*%f",&radius,perimeter);
-    printf("Perimeter of the Circle = %f\n", perimeter);
+    perimeter = 2*3.14*radius;//Formula for perimeter of circle 
+    scanf("%f = 2*3.14*%f",&radius,perimeter);//Taking user input in float data type
+	printf("Perimeter of the Circle = %f\n", perimeter);//Printing the value for perieter of circle
 	
-	area = 3.14*radius*radius;
-	scanf("%f = 3.14*%f*%f",&radius,area);
-	printf("Area of the Circle = %f\n", area);
+	area = 3.14*radius*radius;//Formula for area of circle 
+	scanf("%f = 3.14*%f*%f",&radius,area);//Taking user input in float data type
+	printf("Area of the Circle = %f\n", area);//Printing the value for area of circle
 
-    perimeter = 4*side;
-    scanf("%f = 4*%f",&side,perimeter);
-	printf("Perimeter of the square = %f\n", perimeter);
+    perimeter = 4*side;//Formula for perimeter of square 
+    scanf("%f = 4*%f",&side,perimeter);//Taking user input in float data type
+	printf("Perimeter of the square = %f\n", perimeter);//Printing the value for perieter of sqaure
 	
-	area = side*side;
-	scanf("%f = %f*%f",&side,area);
-	printf("Area of the square = %f\n", area);
+	area = side*side;//Formula for area of square 
+	scanf("%f = %f*%f",&side,area);//Taking user input in float data type
+	printf("Area of the square = %f\n", area);//Printing the value for area of sqaure
 
-	perimeter = 2*(length+breadth);
-	scanf("%f = 2*(%f+%f)",&length,&breadth,perimeter);
-	printf("Perimeter of the rectangle = %f\n", perimeter);
+	perimeter = 2*(length+breadth);//Formula for perimeter of rectangle
+	scanf("%f = 2*(%f+%f)",&length,&breadth,perimeter);//Taking user input in float data type
+	printf("Perimeter of the rectangle = %f\n", perimeter);//Printing the value for perieter of rectangle
 	
-	area = length*breadth;
-	scanf("%f = %f*%f",&length,&breadth,area);
-	printf("Area of the rectangle = %f\n", area);
+	area = length*breadth;//Formula for area of rectangle
+	scanf("%f = %f*%f",&length,&breadth,area);//Taking user input in float data type
+	printf("Area of the rectangle = %f\n", area);//Printing the value for area of rectangle
 
 	return(0);
 }
