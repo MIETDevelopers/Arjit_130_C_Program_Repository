3af3b5e4ed84e7680c9c35d47372524a9761bf79
arjit158 <76278420+arjit158@users.noreply.github.com>
2021-03-30 20:04:41 +0530

    Update read.c

--- read.c
+++ read.c
@@ -21,28 +21,3 @@ int main()
    fclose(fptr); 
   
    return 0;
-=======
-//Author: Arjit manhas
-//Creation Date:29/3/2021
-//Last Modification:29/3/2021
-//Purpose Of The Program:to read a file
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-   char cha[100];
-   FILE *fptr;//declaraing a file type variable
-   if ((fptr = fopen("./txt.txt","r")) == NULL)//opening file in read mode
-   {
-       printf("Error! opening file");
-       exit(1);
-   }
-
-   fscanf(fptr, "%[^\n]", cha);
-   printf("%s", cha);
-   fclose(fptr); 
-  
-   return 0;
->>>>>>> 09c92c853b14b712be994f159b5d29af080cec4f
-}
