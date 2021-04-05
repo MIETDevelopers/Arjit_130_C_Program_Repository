ee0329cbbf4f2aa938a965d7c59a2d294f7e429b
arjit158 <76278420+arjit158@users.noreply.github.com>
2021-04-05 11:53:01 +0530

    Update 31_ReadWriteMarks.c

--- 31_ReadWriteMarks.c
+++ 31_ReadWriteMarks.c
@@ -1,4 +1,4 @@
-//A C program to Program for reading and writing the student marks data to files.				© Ishav Verma 21/March/2021
+//A C program to Program for reading and writing the student marks data to files.				
 #include <stdio.h>
 #include <stdlib.h>
 struct s {
