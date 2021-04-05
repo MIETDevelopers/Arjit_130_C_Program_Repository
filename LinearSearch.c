7961718f98fcbaed52b6630c8fa89bc74eb6725e
arjit158 <76278420+arjit158@users.noreply.github.com>
2021-04-05 17:27:50 +0530

    Update LinearSearch.c

--- LinearSearch.c
+++ LinearSearch.c
@@ -2,33 +2,29 @@
 //Creation Date:22/3/2021
 //Last Modification:22/3/2021
 //Purpose of the program:Searching an element in an array using linear search
-#include <stdio.h>
-    int main(){//Main function body
-        int array[100], search, c, n;//Variable and data type Declaration
-
-        printf("Enter number of elements in array\n");
-        scanf("%d", &n);//Taking user input
-
-        printf("Enter %d integer(s)\n", n);
-
-        for (c = 0; c < n; c++)//Using for loop for increment with c = 0, c < n
-            scanf("%d", &array[c]);
-
-        printf("Enter a number to search\n");
-            scanf("%d", &search);//Taking user input
-
-        for (c = 0; c < n; c++){
-            if (array[c] == search){//If required element is found
-                printf("%d is present at location %d.\n", search, c+1);//printing output if req element is found
-                break;//Break statement
-            } 
-        }
-        if (c == n)//If required element isn't found
-            printf("%d isn't present in the array.\n", search);//printing output if req element isn't found
-
-        return 0;//Return statement
-<<<<<<< HEAD
-      }
-=======
-      }
->>>>>>> fb92279319f09e466a56d6498881a40bd6417583
+#include<stdio.h>
+  
+int main()
+{
+    int a[20],i,x,n;
+    printf("How many elements?");
+    scanf("%d",&n);
+     
+    printf("\nEnter array elements:n");
+    for(i=0;i<n;++i)
+        scanf("%d",&a[i]);
+     
+    printf("\nnEnter element to search:");
+    scanf("%d",&x);
+     
+    for(i=0;i<n;++i)
+        if(a[i]==x)
+            break;
+     
+    if(i<n)
+        printf("\nElement found at index %d",i);
+    else
+        printf("\nElement not found");
+  
+    return 0;
+}
